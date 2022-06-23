792bf2b268e0b5ecd97b672548e3f57614c2d59d
Logan <loganwst.john@gmail.com>
2022-06-22 21:00:20 -0400

    add middleware

--- server.js
+++ server.js
@@ -8,11 +8,28 @@ const dbConnectionString = process.env.DB_STRING
 
 let db,
     dbName = 'sample_mflix',
-    collection
+    collection,
+    PORT
+
+// Connection to database
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
-        console.log(`Connected to Database`)
+        console.log(`Connected to Database ${dbName}`)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+// Middleware
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// Listener
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${PORT || process.env.PORT}`)
+})
